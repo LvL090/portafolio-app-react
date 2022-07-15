36102602d9ff888b0518d8560a36cbd341544447
LvL090 <calvod879@gmail.com>
2022-07-15 15:47:44 +0200

    contact changes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,6 +6,7 @@ const Contact = () => {
         <form className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
                 <p className='text-4xl flex justify-center font-bold inline border-b-4 border-pink-600 text-gray-300'>Contacto</p>
+                <p className=' pt-4 text-4xl flex justify-center text-gray-300'>657424664</p> 
             </div>
             <a href='mailto:calvod879@gmail.com' className='text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center'>
             Conectemos!
